ef714e9dfa686a2171a45b895ad2405dfb374b2c
Guilherme Caciatori <guixy@live.com>
2023-06-06 19:59:31 +0100

    ** atualização das ulas

--- 07.js
+++ 07.js
@@ -7,28 +7,33 @@ const array =[
   ]
 //****** aqui é o find e o findindex */
   const find = array.find((product)=> product.nome === 'celular apple' );
-  console.log(find)
+  //console.log(find)
 
-  const findIndex = array.findIndex((product)=> product.nome === 'celular oppo');
-  console.log(findIndex)
-  console.log(array[findIndex])
+  //const findIndex = array.findIndex((product)=> product.nome === 'celular oppo');
+  //console.log(findIndex)
+  //console.log(array[findIndex])
 
 //**** aqui é o some e o every */
 
 const some = array.some((product)=>product.price > 4000);  //** o .some faz uma pesquisa se tem ou não , sempre retorna com false ou true  */
-console.log(`tem algum produto maior que o valor de 4000 ${some}`)
+//console.log(`tem algum produto maior que o valor de 4000 ${some}`)
 
 const every = array.every((product)=>product.price >= 500);  //** o .every faz uma pesquisa se todos os intens cumpre a regra  , sempre retorna com false ou true  */
-console.log(`todos os produtos da nossa lista são maior que 500  ${every}`)
+//console.log(`todos os produtos da nossa lista são maior que 500  ${every}`)
 
 //** map */
 
 const map = array.map((product)=> {
 return {
-    ...product,
-    subtatoal: product.price * product.quantity
+    ...product,//**aqui nos estamos usando o ...spred para pegar o array primario e jogar no novo junto com o .map  */
+    subtatoal: product.price * product.quantity //**aqui estamos criando um nova categoria subtotal e multiplicando a quantidde pelo valor */
 
 
 }} ); //**o map serve pra pegar um array e modificalo   */
-console.log({map})
+//console.log({map})
 //console.log(`o valor de cada iten é  [${map}]`)
+
+
+const filter = array.filter((product)=> //**aqui estamos usando a função filter , que percorre o nosso array e reponde a nossa regra de negociol  */
+   product.quantity <= 12) //** aqui nos pedimos para ele nos retorna a p produto com a quantidade maior ou igual a 12 em nosso array  */
+console.log(filter);
